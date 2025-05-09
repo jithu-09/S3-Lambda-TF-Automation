eca586409cb54c89de600cd5d70897b9c83d9405
Jitendranadh Kancharla <66712946+jithu-09@users.noreply.github.com>
2025-05-10 03:26:45 +0530

    Update pre-setup-script.sh

--- pre-setup-script.sh
+++ pre-setup-script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define the S3 bucket name
-S3_BUCKET="inbound-bucket-custome"
+S3_BUCKET="bucket-store-j09"
 
 # Create 10 files with the format filename-randomnumber-yyyy-mm-dd
 for i in {1..10}; do
     RANDOM_NUMBER=$((1 + RANDOM % 1000))
     FILENAME="filename-$RANDOM_NUMBER-$(date +%Y-%m-%d).txt"
     echo "This is file number $i" > $FILENAME
-    aws s3 cp $FILENAME s3://$S3_BUCKET/incoming/
+    aws s3 cp $FILENAME s3://$S3_BUCKET/uploads/
 done
 
