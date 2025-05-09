9dfe1246cf9e62fc64fd2d5cb15d77ff166e857b
Jitendranadh Kancharla <66712946+jithu-09@users.noreply.github.com>
2025-05-10 03:39:04 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -41,7 +41,7 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.s3_bucket_policy.arn
+  policy_arn = aws_s3_bucket_policy.s3_bucket_policy.arn
 }
 
 resource "aws_s3_bucket_notification" "lambda_trigger" {
